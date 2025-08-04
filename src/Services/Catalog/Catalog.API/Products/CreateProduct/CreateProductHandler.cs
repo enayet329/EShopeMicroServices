7c9cbfe6d28130c06a7486d204810a0cde9ebbf9
Md Enayet Hossain <md.enayet.hossain329@gmail.com>
2025-08-04 15:05:26 +0600

    Add CreateProduct functionality with initial setup
    Introduces the `Catalog.API.Products.CreateProduct` namespace,
    including the `CreateProductEndpoint` class and the
    `CreateProductHandler` class. Defines `CreateProductCommand`
    and `CreateProductResult` records for product creation.
    The `Handle` method in `CreateProductHandler` is defined but
    not yet implemented, laying the groundwork for future business
    logic.

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+
+public record CreateProductCommand(
+	string Name,
+	string Description,
+	decimal Price,
+	string Category,
+	string ImageUrl
+) : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+	public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+	{
+		// business logic to create product
+		throw new NotImplementedException();
+	}
+}
