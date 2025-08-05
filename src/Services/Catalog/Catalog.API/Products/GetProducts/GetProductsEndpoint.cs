6d6b87c55f24b6343730ecd20dd4313de23cf0db
MD Anayet Hossen <mdsakayethossen2@gmail.com>
2025-08-05 15:30:33 +0600

    feat: add get all product endpoint using carter modul package

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,5 +1,20 @@
 ﻿namespace Catalog.API.Products.GetProducts;
 
-public class GetProductsEndpoint
+public record GetProductsResponse(IEnumerable<Product> Products);
+public class GetProductsEndpoint : ICarterModule
 {
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapGet("/products", async (ISender sender) =>
+		{
+			var products = await sender.Send(new GetProductsQuery());
+			var response = products.Adapt<GetProductsResponse>();
+			return Results.Ok(response);
+		})
+		.WithName("GetProducts")
+		.Produces<GetProductsResponse>(StatusCodes.Status201Created)
+		.ProducesProblem(StatusCodes.Status400BadRequest)
+		.WithSummary("Get Products")
+		.WithDescription("Get Products");
+	}
 }
