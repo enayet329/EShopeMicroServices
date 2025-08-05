af3da01bb31e416f2b3080a1f536662d718e257b
MD Anayet Hossen <mdsakayethossen2@gmail.com>
2025-08-05 21:34:00 +0600

    feat: add new endpoint for get product by category

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapGet("/products/{category}", async (string category, ISender sender) =>
+		{
+			var products = await sender.Send(new GetProductByCategoryQuery(category));
+
+			var result = products.Adapt<GetProductByCategoryResult>();
+			return Results.Ok(result);
+
+		})
+		.WithName("GetProductsByCategory")
+		.Produces<GetProductByCategoryResult>(StatusCodes.Status201Created)
+		.ProducesProblem(StatusCodes.Status400BadRequest)
+		.WithSummary("Get Products By Category")
+		.WithDescription("Get Products By Category");
+	}
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+
+public record GetProductByCategoryQuery(string category) : IQuery<GetProductByCategoryResponse>;
+
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResponse>
+{
+
+	public async Task<GetProductByCategoryResponse> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+	{
+		logger.LogInformation("Handling GetProductByCategoryQuery for category {Category}", query.category);
+
+		var products = await session.Query<Product>()
+			.Where(p => p.Category.Contains(query.category))
+			.ToListAsync(cancellationToken);
+
+		logger.LogInformation("Retrieved {Count} products for category {Category}", products.Count, query.category);
+
+		return new GetProductByCategoryResponse(products);
+	}
+}
