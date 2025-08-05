4189b19459e0b712935f0773ed101eadb4016178
MD Anayet Hossen <mdsakayethossen2@gmail.com>
2025-08-05 21:52:45 +0600

    feat: add new api for update product

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,29 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductRequest(
+Guid Id,
+string Name,
+string Description,
+decimal Price,
+List<string> Category,
+string ImageFile);
+
+public record UpdateProductResponse(bool IsSuccess);
+
+public class UpdateProductEndpoint : ICarterModule
+{
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapPut("/products",async (UpdateProductRequest request, ISender sender) =>
+			{
+				var product = request.Adapt<UpdateProductCommand>();
+				var result = await sender.Send(product);
+
+				return new UpdateProductResponse(result.IsSuccess);
+			})
+			.WithName("UpdateProduct")
+			.Produces<UpdateProductResponse>()
+			.Produces(StatusCodes.Status400BadRequest)
+			.WithDescription("Update Product");
+	}
+}
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(
+	Guid Id,
+	string Name,
+	string Description,
+	decimal Price,
+	List<string> Category,
+	string ImageFile) : ICommand<UpdateProductResult>;
+
+public record UpdateProductResult(bool IsSuccess);
+
+public class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+	: ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+	public async Task<UpdateProductResult> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+	{
+		logger.LogInformation("Handling UpdateProductCommand for product with ID: {ProductId}", request.Id);
+
+		var product = await session.LoadAsync<Product>(request.Id, cancellationToken);
+		if (product is null)
+		{
+			throw new ProductNotFoundException();
+		}
+
+		product.Name = request.Name;
+		product.Description = request.Description;
+		product.Price = request.Price;
+		product.Category = request.Category;
+		product.ImageFile = request.ImageFile;
+
+		session.Store(product);
+		await session.SaveChangesAsync(cancellationToken);
+
+		return new UpdateProductResult(true);
+	}
+}
