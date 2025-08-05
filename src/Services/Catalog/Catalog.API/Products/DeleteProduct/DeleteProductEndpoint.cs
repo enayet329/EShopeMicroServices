0e6df2a162fb71fd908fa9c3e70c20956cb074ff
MD Anayet Hossen <mdsakayethossen2@gmail.com>
2025-08-05 22:08:20 +0600

    feat: add new api endpoint for delete product by id

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductResponse(bool IsSuccess);
+
+public class DeleteProductEndpoint : ICarterModule
+{
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapDelete("/products/{productId:guid}", async (Guid productId, ISender sender) =>
+		{
+			var result = await sender.Send(new DeleteProductCommand(productId));
+
+			return Results.Ok(new DeleteProductResponse(result.IsSuccess));
+		})
+		.WithName("DeleteProductsById")
+		.Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+		.ProducesProblem(StatusCodes.Status400BadRequest)
+		.WithSummary("Delete Product By Id")
+		.WithDescription("Delete Product By Id");
+	}
+}
---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid ProductId) : ICommand<DeleteProductResult>;
+public record DeleteProductResult(bool IsSuccess);
+
+public class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
+	: ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+	public async Task<DeleteProductResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+	{
+		logger.LogInformation("Handling DeleteProductCommand for product with ID: {ProductId}", request.ProductId);
+
+		session.Delete<Product>(request.ProductId);
+
+		await session.SaveChangesAsync(cancellationToken);
+
+		return new DeleteProductResult(true);
+	}
+}
