5e4628bd0116f1ce785193a554f31fed1062d6c0
Md Enayet Hossain <md.enayet.hossain329@gmail.com>
2025-08-06 13:24:11 +0600

    Add Carter integration and refactor basket handling
    Updated `Basket.API.csproj` to include the Carter package and a project reference to BuildingBlocks. Introduced `GetBasketEndpoint` for handling basket retrieval with a new response model. Refactored `GetBasketHandler` to `GetBasketQueryHandler`, implementing the `IQueryHandler` interface and returning a structured result. Added global using directives for improved code organization.

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -1,5 +1,24 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketEndpoint
+// public record GetBasketQuery(string UserName);
+
+public record GetBasketResponse(ShoppingtCart Cart);
+
+public class GetBasketEndpoint : ICarterModule
 {
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+		{
+			var result = await sender.Send(new GetBasketQuery(userName));
+
+			var response = result.Adapt<GetBasketResponse>();
+			return Results.Ok(response);
+		})
+		.WithName("GetBasket")
+		.Produces<GetBasketResponse>(StatusCodes.Status201Created)
+		.ProducesProblem(StatusCodes.Status400BadRequest)
+		.WithSummary("Get Basket")
+		.WithDescription("Get Basket");
+	}
 }
--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,5 +1,15 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketHandler
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingtCart Cart);
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
+	public async Task<GetBasketResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+	{
+		// TODO: get basket from database
+		// var basket = await _basketRepository.GetBasketAsync(request.UserName, cancellationToken);
+		return new GetBasketResult(new ShoppingtCart("test"));
+	}
 }
