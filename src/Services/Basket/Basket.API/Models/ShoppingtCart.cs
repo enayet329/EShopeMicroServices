0650d724e1be98dc154564a5dad5440a1c74ae60
Md Enayet Hossain <md.enayet.hossain329@gmail.com>
2025-08-06 12:59:05 +0600

    Add ShoppingCartItem and ShoppingtCart classes
    Introduced `ShoppingCartItem` class with properties for
    quantity, color, price, product ID, and product name.
    Added `ShoppingtCart` class with user name, a list of
    shopping cart items, and a computed property for total
    price. Included constructors for initializing the cart.

---
+++ src/Services/Basket/Basket.API/Models/ShoppingtCart.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingtCart
+{
+	public string UserName { get; set; } = default!;
+	public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+	public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+	public ShoppingtCart(string userName)
+	{
+		UserName = userName;
+	}
+
+	public ShoppingtCart()
+	{
+	}
+}
