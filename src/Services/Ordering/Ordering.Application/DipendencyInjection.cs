0978dded7b7b91a47c8de4a72b9b71e19f43b1e2
Md Enayet Hossain <md.enayet.hossain329@gmail.com>
2025-08-23 12:15:46 +0600

    Add Dependency Injection structure for Ordering microservices
    Introduce Dependency Injection classes for API, Application, and Infrastructure layers. Update project files with necessary references and target framework settings. Modify solution file to include new projects and configurations for a complete microservices architecture.

---
+++ src/Services/Ordering/Ordering.Application/DipendencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application;
+
+public static class DipendencyInjection
+{
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	{
+		//services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+		return services;
+	}
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/DipendencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure;
+
+public static class DipendencyInjection
+{
+	public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+	{
+		var connectionString = configuration.GetConnectionString("Database");
+		// Add Services to the container.
+		//services.AddDbContext<OrderingContext>(options => options.UseSqlServer(connectionString));
+
+		//services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+		return services;
+	}
+
+}
